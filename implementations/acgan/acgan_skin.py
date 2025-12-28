112cc98c252dca22b99b3a923f493e3c9b0f5f72
Eduardo Pérez, PhD <eperez15@us.es>
2025-12-28 21:41:52 +0100

    add docs

--- implementations/acgan/acgan_skin.py
+++ implementations/acgan/acgan_skin.py
@@ -21,7 +21,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=int(os.getenv("n_epochs", 200)), help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=int(os.getenv("batch_size", 64)), help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
--- readme.md
+++ readme.md
@@ -6,3 +6,17 @@ docker run \
     -v /home/eperezp1990/.temp:/home/eperez \
     --name pytorch-2 -dit eperezp1990/pytorch:2.2.0-cuda11.4
 ```
+
+# Execute experiment
+
+```
+export PYTHONPATH=.
+nohup python3 acgan/acgan_skin.py &
+```
+
+# Copy results
+
+```
+scp -r Tesla:/home/eperezp1990/.temp/gans/acgan/images/0799.png images/
+scp -r Tesla:/home/eperezp1990/.temp/gans/acgan/images/0799/00000.png images/
+```
