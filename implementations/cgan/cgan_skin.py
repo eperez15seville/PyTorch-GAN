280f161292cdcc935544fdf4cfe9a3cb05d00229
Eduardo Pérez, PhD <eperez15@us.es>
2026-01-15 16:01:47 +0100

    end cgan

--- implementations/cgan/cgan_skin.py
+++ implementations/cgan/cgan_skin.py
@@ -1,39 +1,81 @@
 import argparse
 import os
 import numpy as np
-import math
 
-import torchvision.transforms as transforms
-from torchvision.utils import save_image
 
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torch.autograd import Variable
 
 import torch.nn as nn
-import torch.nn.functional as F
 import torch
 
-os.makedirs("images", exist_ok=True)
+import util
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+parser.add_argument(
+    "--n_epochs",
+    type=int,
+    default=int(os.getenv("n_epochs", 200)),
+    help="number of epochs of training",
+)
+parser.add_argument(
+    "--batch_size", type=int, default=int(os.getenv("batch_size", 64)), help="size of the batches"
+)
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
-parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
-parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+parser.add_argument(
+    "--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient"
+)
+parser.add_argument(
+    "--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient"
+)
+parser.add_argument(
+    "--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation"
+)
+parser.add_argument(
+    "--latent_dim", type=int, default=100, help="dimensionality of the latent space"
+)
+parser.add_argument(
+    "--n_classes",
+    type=int,
+    default=int(os.getenv("n_classes", 10)),
+    help="number of classes for dataset",
+)
+parser.add_argument(
+    "--img_size",
+    type=int,
+    default=int(os.getenv("img_size", 32)),
+    help="size of each image dimension",
+)
+parser.add_argument(
+    "--channels", type=int, default=int(os.getenv("channels", 1)), help="number of image channels"
+)
+parser.add_argument(
+    "--sample_interval", type=int, default=400, help="interval between image sampling"
+)
+parser.add_argument(
+    "--data_path",
+    type=str,
+    default=os.getenv("data_path", ""),
+    help="path to dataset folder with subfolders as labels",
+)
+parser.add_argument(
+    "--results_path",
+    type=str,
+    default=os.getenv("results_path", ""),
+    help="path to dataset folder with subfolders as labels",
+)
+
 opt = parser.parse_args()
 print(opt)
 
-img_shape = (opt.channels, opt.img_size, opt.img_size)
-
 cuda = True if torch.cuda.is_available() else False
+
+img_shape = (opt.channels, opt.img_size, opt.img_size)
 
 
 class Generator(nn.Module):
@@ -55,7 +97,7 @@ class Generator(nn.Module):
             *block(256, 512),
             *block(512, 1024),
             nn.Linear(1024, int(np.prod(img_shape))),
-            nn.Tanh()
+            nn.Tanh(),
         )
 
     def forward(self, noise, labels):
@@ -104,18 +146,26 @@ if cuda:
     adversarial_loss.cuda()
 
 # Configure data loader
-os.makedirs("../../data/mnist", exist_ok=True)
-dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST(
-        "../../data/mnist",
-        train=True,
-        download=True,
-        transform=transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-        ),
-    ),
+dataset_init = datasets.ImageFolder(
+    root=opt.data_path, transform=util.custom_preprocessing(opt), is_valid_file=util.is_valid_file
+)
+
+# find the classes to augment
+to_aug = util.get_number_instances_to_aug(dataset_init)
+
+# this CGAN learns from all classes
+# we are not generating the greatest classes only (nevus, for example)
+dataset = datasets.ImageFolder(
+    root=opt.data_path,
+    transform=util.custom_preprocessing(opt),
+    is_valid_file=util.is_valid_file,
+)
+
+dataloader = DataLoader(
+    dataset,
     batch_size=opt.batch_size,
     shuffle=True,
+    num_workers=opt.n_cpu,
 )
 
 # Optimizers
@@ -124,18 +174,6 @@ optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt
 
 FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
-
-
-def sample_image(n_row, batches_done):
-    """Saves a grid of generated digits ranging from 0 to n_classes"""
-    # Sample noise
-    z = Variable(FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
-    # Get labels ranging from 0 to n_classes for n rows
-    labels = np.array([num for _ in range(n_row) for num in range(n_row)])
-    labels = Variable(LongTensor(labels))
-    gen_imgs = generator(z, labels)
-    save_image(gen_imgs.data, "images/%d.png" % batches_done, nrow=n_row, normalize=True)
-
 
 # ----------
 #  Training
@@ -200,5 +238,11 @@ for epoch in range(opt.n_epochs):
         )
 
         batches_done = epoch * len(dataloader) + i
-        if batches_done % opt.sample_interval == 0:
-            sample_image(n_row=10, batches_done=batches_done)
+
+    # for each epoch, save sample images
+    util.sample_image(
+        to_aug=to_aug,
+        latent_dim=opt.latent_dim,
+        generator=generator,
+        results_path=opt.results_path,
+    )
--- readme.md
+++ readme.md
@@ -23,3 +23,27 @@ nohup python3 acgan/acgan_skin.py &
 scp -r Tesla:/home/eperezp1990/.temp/gans/acgan/images/0799.png images/
 scp -r Tesla:/home/eperezp1990/.temp/gans/acgan/images/0799/00000.png images/
 ```
+
+# Experimental study
+
+No categories:
+
+```
+aae/
+began/
+bgan/
+```
+
+Categories:
+
+```
+acgan/
+cgan/
+```
+
+Issues:
+
+```
+bicyclegan/
+ccgan/
+```
